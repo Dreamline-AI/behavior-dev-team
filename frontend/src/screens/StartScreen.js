5c35fb13f97dddbb8b28c1365d511ab344f54a00
saeb3 <114490373+saeb3@users.noreply.github.com>
2024-06-25 21:50:34 -0400

    StartScreen Update

--- frontend/src/screens/StartScreen.js
+++ frontend/src/screens/StartScreen.js
@@ -1,69 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Background from '../components/Background';
-import Logo from '../components/Logo';
-import Header from '../components/Header';
-import Button from '../components/Button';
-import TextInput from '../components/TextInput';
-import { emailValidator } from '../helpers/emailValidator';
-import { passwordValidator } from '../helpers/passwordValidator';
-import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import React, { useState, useEffect } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import TextInput from '../components/TextInput'
+import { emailValidator } from '../helpers/emailValidator'
+import { passwordValidator } from '../helpers/passwordValidator'
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 import { signInWithGooglePopup } from '../../firebaseConfig'
 import { signInWithFacebookPopup } from '../../firebaseConfig'
 
 export default function StartScreen({ navigation }) {
-  const [email, setEmail] = useState({ value: '', error: '' });
-  const [password, setPassword] = useState({ value: '', error: '' });
-  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [email, setEmail] = useState({ value: '', error: '' })
+  const [password, setPassword] = useState({ value: '', error: '' })
+  const [isEmailValid, setIsEmailValid] = useState(false)
 
   useEffect(() => {
-    setIsEmailValid(!emailValidator(email.value));
-  }, [email.value]);
+    setIsEmailValid(!emailValidator(email.value))
+  }, [email.value])
 
   const onLoginPressed = () => {
-    const emailError = emailValidator(email.value);
-    const passwordError = passwordValidator(password.value);
+    const emailError = emailValidator(email.value)
+    const passwordError = passwordValidator(password.value)
     if (emailError || passwordError) {
-      setEmail({ ...email, error: emailError });
-      setPassword({ ...password, error: passwordError });
-      return;
+      setEmail({ ...email, error: emailError })
+      setPassword({ ...password, error: passwordError })
+      return
     }
     navigation.reset({
       index: 0,
       routes: [{ name: 'Dashboard' }],
-    });
-  };
+    })
+  }
 
-  const [isFirstTimeSignIn, setIsFirstTimeSignIn] = useState(true);
+  const [isFirstTimeSignIn, setIsFirstTimeSignIn] = useState(true)
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log('response-->', response);
+    const response = await signInWithGooglePopup()
+    console.log('response-->', response)
 
     if (await response?.user?.getIdToken()) {
       // Check if it's the first time sign-in
-    if (isFirstTimeSignIn) {
-      setIsFirstTimeSignIn(false); 
-      navigation.navigate('SignUpForm');
+      if (isFirstTimeSignIn) {
+        setIsFirstTimeSignIn(false)
+        navigation.navigate('SignUpForm')
+      } else {
+        // Navigate to a different screen for returning users
+        navigation.navigate('Dashboard')
+      }
     } else {
-      // Navigate to a different screen for returning users
-      navigation.navigate('Dashboard');
+      // Handle sign-in failure
     }
-  } else {
-    // Handle sign-in failure
   }
-};
-const logFBUser = async () => {
+  const logFBUser = async () => {
+    const response = await signInWithFacebookPopup()
+    console.log('response-->', response)
 
-  const response = await signInWithFacebookPopup();
-  console.log('response-->', response);
-
-  if (await response?.user?.getIdToken()) {
-    navigation.navigate('SignUpForm');
-  } else {
-    // back to login or signup screen
+    if (await response?.user?.getIdToken()) {
+      navigation.navigate('SignUpForm')
+    } else {
+      // back to login or signup screen
+    }
   }
-}
   return (
     <Background>
       <Header>Sign up or Sign in</Header>
@@ -80,14 +79,13 @@ const logFBUser = async () => {
         autoCompleteType="email"
         textContentType="emailAddress"
         keyboardType="email-address"
-       
       />
 
       <Button
-        color={isEmailValid ? "black" : "gray"}
+        color={isEmailValid ? 'black' : 'gray'}
         mode="contained"
         disabled={!isEmailValid}
-        onPress={() => navigation.navigate('LoginWithGFA')}
+        onPress={() => navigation.navigate('EmailSignUp')}
       >
         Continue
       </Button>
@@ -99,37 +97,57 @@ const logFBUser = async () => {
       </View>
 
       <Button
-        color='white'
+        color="white"
         mode="contained"
         onPress={logGoogleUser}
         style={styles.buttonBorder}
-        icon={() => <AntDesign name="google" size={20} color="black" style={styles.iconStyle} />}
+        icon={() => (
+          <AntDesign
+            name="google"
+            size={20}
+            color="black"
+            style={styles.iconStyle}
+          />
+        )}
       >
         <Text style={styles.buttonText}>Continue with Google</Text>
       </Button>
-      
+
       <Button
-        color='white'
+        color="white"
         mode="contained"
         onPress={logFBUser}
         style={styles.buttonBorder}
-        icon={() => <MaterialIcon name="facebook" size={20} color="black" style={styles.iconStyle} />}
+        icon={() => (
+          <MaterialIcon
+            name="facebook"
+            size={20}
+            color="black"
+            style={styles.iconStyle}
+          />
+        )}
       >
         <Text style={styles.buttonText}>Continue with Facebook</Text>
       </Button>
-      
+
       <Button
-        color='white'
+        color="white"
         mode="contained"
         onPress={() => navigation.navigate('LoginWithGFA')}
         style={styles.buttonBorder}
-        icon={() => <MaterialIcon name="apple" size={20} color="black" style={styles.iconStyle} />}
+        icon={() => (
+          <MaterialIcon
+            name="apple"
+            size={20}
+            color="black"
+            style={styles.iconStyle}
+          />
+        )}
       >
         <Text style={styles.buttonText}>Continue with Apple</Text>
       </Button>
-      
     </Background>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -152,18 +170,17 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     borderRadius: 8,
-    flexDirection: 'row', 
-    alignItems: 'center', 
+    flexDirection: 'row',
+    alignItems: 'center',
     marginVertical: 5,
   },
   iconStyle: {
-    marginRight: 10, 
+    marginRight: 10,
   },
   buttonText: {
     flex: 1,
     textAlign: 'center',
-    fontSize: 16, 
-    fontWeight: 'bold'
+    fontSize: 16,
+    fontWeight: 'bold',
   },
- 
-});
+})
