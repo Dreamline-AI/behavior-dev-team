0dcf5d66313158c75a4016b451f0dfd7909ef720
Nikitha Cherukuthota <nikitha.ch18@gmail.com>
2024-06-27 23:23:35 +0530

    Backend code run

--- mvp/src/main/java/com/sustainability/mvp/MvpApplication.java
+++ mvp/src/main/java/com/sustainability/mvp/MvpApplication.java
@@ -1,18 +1,19 @@
 package com.sustainability.mvp;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.core.io.ClassPathResource;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.core.io.ClassPathResource;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MvpApplication {
@@ -25,7 +26,7 @@ public class MvpApplication {
 
 		try {
 			FileInputStream serviceAccount =
-					new FileInputStream("path/to/serviceAccountKey.json");
+					new FileInputStream("/Users/nikit/DreamLine/behavior-dev-team/mvp/src/main/resources/serviceAccountKey.json");
 
 			FirebaseOptions options = new FirebaseOptions.Builder()
 					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
