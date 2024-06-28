c99b1280a11319241389eb94b0fad1a7a5ea6447
acrum001 <crum.andrew@gmail.com>
2024-06-28 11:54:09 -0400

    added docker for backend.

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # behavior-dev-team
 
-How to run?
-1. Download or clone this repo
+## Frontend
+Standard is to use `yarn`. Avoid `npm`, unless compelling use-case.
+The frontend is in `./frontend` directory. To run development:
+
+```
+cd ./frontend
+yarn install
+yarn web
+```
+
+### iOS
+```yarn ios```
+
+### Android
+```yarn android```
+
+
+
+
 2. Install dependencies.
 npm install
 // or
@@ -10,3 +27,15 @@ yarn install
  npm run ios // npm run android // npm run web
  // or
  yarn ios // yarn android
+
+
+## Backend
+The backend is in the `./mvp` directory and can be deployed locally with these commands:
+
+```
+cd mvp
+docker build -t mvp:local .
+docker run -p 8080:8080 -t mvp:local
+```
+
+Any change to files in ./mvp will need a `docker build`, because the Dockerfile compiles java to app.jar, copies that to the docker image. The docker image can be started/stopped to bring up a server.
--- mvp/src/main/java/com/sustainability/mvp/firebase/FirebaseInitialization.java
+++ mvp/src/main/java/com/sustainability/mvp/firebase/FirebaseInitialization.java
@@ -1,12 +1,7 @@
 package com.sustainability.mvp.firebase;
 
-import java.io.FileInputStream;
-
 import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 import com.google.auth.oauth2.GoogleCredentials;
@@ -16,31 +11,24 @@ import com.google.firebase.FirebaseOptions;
 @Service
 public class FirebaseInitialization {
 
-    @Autowired
-    private ResourceLoader resourceLoader;
-
     @PostConstruct
     public void initialization() {
-
-        FileInputStream serviceAccount = null;
-
         try {
-            Resource fileResource = resourceLoader.getResource("classpath:serviceAccountKey.json");
-            serviceAccount = new FileInputStream(fileResource.getFile());
+            String googleApplicationCredentials = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+            if (googleApplicationCredentials == null) {
+                throw new IllegalStateException("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.");
+            }
 
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(credentials)
                     .build();
 
-           if (FirebaseApp.getApps().isEmpty()) {
-				FirebaseApp.initializeApp(options);
-			}
-        }
-        catch (Exception e) {
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (Exception e) {
             throw new IllegalStateException("Firebase initialization failed", e);
         }
-
-
-
     }
 }
