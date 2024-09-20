992aaf7a612dd429ac175588d55288394900d9b9
saeb3 <114490373+saeb3@users.noreply.github.com>
2024-09-19 21:20:20 -0400

    Edit Button fix

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -1,11 +1,13 @@
-
-
 export const loginSuccess = (user) => ({
-    type: 'LOGIN_SUCCESS',
-    payload: user,
-  });
-  
-  export const logout = () => ({
-    type: 'LOGOUT',
-  });
-  
+  type: 'LOGIN_SUCCESS',
+  payload: user,
+})
+
+export const logout = () => ({
+  type: 'LOGOUT',
+})
+
+export const updateUserInfo = (user) => ({
+  type: 'UPDATE_USER_INFO',
+  payload: user,
+})
--- frontend/src/screens/EditProfileScreen.js
+++ frontend/src/screens/EditProfileScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, StyleSheet, TouchableOpacity } from 'react-native'
 import Background from '../components/Background'
 import Header from '../components/Header'
@@ -7,18 +7,46 @@ import TextInput from '../components/TextInput'
 import BackButton from '../components/BackButton'
 import { theme } from '../core/theme'
 import styles from '../commonStyles'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateUserInfo } from '../actions/authActions'
 
+export default function EditProfileScreen({ route, navigation }) {
+  // Destructure name from route.params with a default empty string
+  const { name = '' } = route.params || {}
 
-export default function EditProfileScreen({route, navigation }){
-    const { name } = route.params;
-    const [fn, ...ln] = name.split(' ');
-    const [firstName, setFirstName] = useState({value : fn, error : ''})
-    const [lastName, setLastName] = useState({value : ln.join(' '), error :''})
-    const [zipcode, setZipcode] = useState({value : '', error : ''})
-    const onSaveChangesPressed = () =>{
-        const userName = firstName.value+" "+lastName.value;
-        navigation.navigate('ProfileScreen', { userName });
+  // Ensure that name is a string and split safely
+  const [fn, ...ln] = typeof name === 'string' ? name.split(' ') : []
+
+  const [firstName, setFirstName] = useState({ value: fn, error: '' })
+  const [lastName, setLastName] = useState({ value: ln.join(' '), error: '' })
+  const [zipcode, setZipcode] = useState({ value: '', error: '' })
+
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.auth.user)
+
+  useEffect(() => {
+    if (user) {
+      setFirstName({ value: user.firstName, error: '' })
+      setLastName({ value: user.lastName, error: '' })
+      setZipcode({ value: user.zipCode, error: '' }) // Prefill with existing zipcode
     }
+  }, [user])
+
+  const onSaveChangesPressed = () => {
+    // Dispatch the action to update user info
+    const userName = `${firstName.value} ${lastName.value}`.trim()
+
+    dispatch(
+      updateUserInfo({
+        firstName: firstName.value,
+        lastName: lastName.value,
+        zipCode: zipcode.value,
+        userName: userName,
+      })
+    )
+
+    navigation.navigate('ProfileScreen')
+  }
 
   return (
     <Background>
