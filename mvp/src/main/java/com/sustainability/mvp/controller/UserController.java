07cf26f9e8ccda6a6e0c0850a0166164fd550a3d
vaisaali-96 <113218119+vaisaali-96@users.noreply.github.com>
2024-10-05 08:07:46 -0700

    Backend check for existing user

--- mvp/src/main/java/com/sustainability/mvp/controller/UserController.java
+++ mvp/src/main/java/com/sustainability/mvp/controller/UserController.java
@@ -6,15 +6,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.sustainability.mvp.entity.User;
 import com.sustainability.mvp.service.UserService;
@@ -50,5 +42,8 @@ public class UserController {
     public String deleteUserByUserID(@PathVariable String userID) throws ExecutionException, InterruptedException {
         return userService.deleteUserByUserID(userID);
     }
-
+    @GetMapping("/users/email")
+    public User getUserByEmail(@RequestParam String email) throws ExecutionException, InterruptedException {
+        return userService.getUserByEmail(email);
+    }
 }
